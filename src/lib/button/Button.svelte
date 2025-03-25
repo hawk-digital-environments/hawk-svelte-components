22b7d17270192f9d7baecb6063b404ec46563bc8
Jonas Trippler <38106129+jonesvan@users.noreply.github.com>
2025-03-25 11:16:04 +0100

    added button

--- src/lib/button/Button.svelte
+++ src/lib/button/Button.svelte
@@ -1,29 +1,63 @@
 <script lang="ts">
-  import './button.css';
+  import style from "./button.module.css";
+  import classNames from "classnames";
 
   interface Props {
-    /** Is this the principal call to action on the page? */
-    primary?: boolean;
-    /** What background color to use */
-    backgroundColor?: string;
-    /** How large should the button be? */
-    size?: 'small' | 'medium' | 'large';
-    /** Button contents */
-    label: string;
-    /** The onclick event handler */
+    // does cool stuff with colors
+    type?: "filled" | "outlined" | "text" | "link" | "raised";
+    /** Hello World */
+    size?: "small" | "large";
+    label?: string;
+    icon?: string;
+    iconPosition?: "left" | "right";
+    iconOnly?: boolean;
+    link?: string;
+    linkTarget?: string;
+    disabled: boolean;
     onClick?: () => void;
   }
-  
-  const { primary = false, backgroundColor, size = 'medium', label, onClick }: Props = $props();
+
+  const {
+    type = "filled",
+    size = "small",
+    label = "Button",
+    icon = "+",
+    iconPosition = "left",
+    iconOnly = false,
+    link = "",
+    linkTarget = "_blank",
+    disabled = false,
+    onClick,
+  }: Props = $props();
+
+  const buttonClasses = $derived(
+    classNames(style.button, {
+      [style.sizeLarge]: size === "large",
+      [style.sizeSmall]: size === "small",
+
+      [style.typeFilled]: type === "filled",
+      [style.typeOutlined]: type === "outlined",
+      [style.typeText]: type === "text",
+      [style.typeLink]: type === "link",
+      [style.typeRaised]: type === "raised",
+
+      [style.iconPositionLeft]: iconPosition === "left",
+      [style.iconPositionRight]: iconPosition === "right",
+      [style.iconOnly]: iconOnly,
+
+      [style.disabled]: disabled,
+    }),
+  );
 </script>
 
-<button
-  type="button"
-  class={['storybook-button', `storybook-button--${size}`].join(' ')}
-  class:storybook-button--primary={primary}
-  class:storybook-button--secondary={!primary}
-  style:background-color={backgroundColor}
-  onclick={onClick}
->
-  {label}
-</button>
+{#if link}
+  <a href={link} target={linkTarget} class={buttonClasses}>
+    <span class="icon">{icon}</span>
+    <span class="label">{label}</span>
+  </a>
+{:else}
+  <button type="button" class={buttonClasses} {disabled} onclick={onClick}>
+    <span class="icon">{icon}</span>
+    <span class="label">{label}</span>
+  </button>
+{/if}
