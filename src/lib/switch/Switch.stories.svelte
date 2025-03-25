5657d513dbbcb039891086d48ca3f92dc4e0dcf3
Jonas Trippler <38106129+jonesvan@users.noreply.github.com>
2025-03-25 11:21:39 +0100

    initial Switch

---
+++ src/lib/switch/Switch.stories.svelte
@@ -0,0 +1,17 @@
+<script module>
+  import { defineMeta } from "@storybook/addon-svelte-csf";
+  import Button from "./Switch.svelte";
+  import { fn } from "@storybook/test";
+
+  const { Story } = defineMeta({
+    title: "HAWK/Switch",
+    component: Button,
+    tags: ["autodocs"],
+    argTypes: {},
+    args: {
+      onClick: fn(),
+    },
+  });
+</script>
+
+<Story name="Primary" args={{}} />
---
+++ src/lib/switch/Switch.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import style from "./switch.module.css";
+  import classNames from "classnames";
+
+  interface Props {
+    checked?: boolean;
+    disabled?: boolean;
+  }
+
+  const { checked = false, disabled = false }: Props = $props();
+</script>
+
+<input type="checkbox" role="switch" {checked} />
