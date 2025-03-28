fb24bf74d60a1268244d100efbaae7e321e8d1c8
Neunerlei <code@neunerlei.eu>
2025-03-28 15:05:45 +0100

    feat: finalize switch component

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,4 @@
 export {default as Button} from './button/Button.svelte';
 export {default as Icon} from './icon/Icon.svelte';
-export {default as Link} from './link/Link.svelte';
+export {default as Link} from './util/link/Link.svelte';
+export {default as Switch} from './switch/Switch.svelte';
