2cd1a59878fb23831b208d1c176f63e5a1e1ca50
Martin Neundorfer <code@neunerlei.eu>
2025-05-21 17:17:44 +0200

    fix: minor adjustments and bugfixes + additional stories for edge cases (#11)
    * fix: minor adjustments and bugfixes + additional stories for edge cases
    
    SEE VIRTU-90, VIRTU-94, VIRTU-89, VIRTU-88
    
    * docs: add resolution link to link target prop

--- src/lib/util/link/Link.svelte
+++ src/lib/util/link/Link.svelte
@@ -12,7 +12,8 @@
         href?: string;
 
         /**
-         * The target of the link, defaults to "_self"
+         * The target of the link, defaults to an empty string -> browser default
+         * @see https://github.com/sveltejs/sapper/issues/265 --> _self means ALWAYS reload for sveltekit!
          */
         target?: string;
 
@@ -39,7 +40,7 @@
 
     const {
         href: hrefRaw = '',
-        target = '_self',
+        target = '',
         rel: relRaw = '',
         onclick: onclickRaw,
         children,
@@ -73,15 +74,27 @@
         }
         return onclickRaw;
     })
+
+    const dynamicProps = $derived.by(() => {
+        const props: Record<string, any> = {};
+        if (target) {
+            props.target = target;
+        }
+        if (rel) {
+            props.rel = rel;
+        }
+        if (onclick) {
+            props.onclick = onclick;
+        }
+        return props;
+    });
 </script>
 
 <a href="{href}"
    class={classNames}
    class:link={true}
    class:disabled={disabled}
-   target="{target}"
-   rel="{rel}"
-   onclick="{onclick}"
+   {...dynamicProps}
    {...restProps}>
     {@render children?.()}
 </a>
