8f6a9effa415e1a5365f51e8d8a48db3d99ed7c4
neunerlei <ich@neunerlei.eu>
2025-06-06 16:44:47 +0200

    refactor(CustomElements): work with divs for slotted snippet instead of spans

--- src/lib/util/makeCustomComponent.ts
+++ src/lib/util/makeCustomComponent.ts
@@ -30,33 +30,30 @@ import {createRawSnippet, type Snippet} from 'svelte';
  */
 export function makeCustomComponent(baseClass: new () => HTMLElement): new () => HTMLElement {
     return class CustomComponent extends baseClass {
-        private readonly _childStorage: HTMLSpanElement;
-        private readonly _snippetStorage: Record<string, HTMLSpanElement> = {};
+        private readonly _snippetStorage: Record<string, HTMLDivElement> = {};
 
         constructor() {
             super();
-            this._childStorage = document.createElement('span');
-            this._childStorage.append(...this.childNodes);
+            const childStorage = this._getStorage('children');
+            childStorage.append(...this.childNodes);
 
             // Extract all snippets into their own dedicated storage
-            const snippets = this._childStorage.getElementsByTagName('hawk-snippet');
-            for (const snippet of snippets) {
+            for (const snippet of childStorage.querySelectorAll('& > hawk-snippet')) {
                 const name = snippet.getAttribute('property') || '';
                 if (name) {
-                    if (!this._snippetStorage[name]) {
-                        this._snippetStorage[name] = document.createElement('span');
-                        (this as any)[name] = propertySnippetFactory(this._snippetStorage[name]);
-                    }
-                    this._snippetStorage[name].append(...snippet.childNodes);
+                    console.log('Storing snippet under property:', name, [...snippet.childNodes], 'of', this);
+                    this._getStorage(name).append(...snippet.childNodes);
                 } else {
                     console.warn('Snippet without property attribute found:', snippet);
-                    snippet.remove();
                 }
+                snippet.remove();
             }
+
+            console.log(this._snippetStorage);
         }
 
         get children() {
-            const mixedSnippet = propertySnippetFactory(this._childStorage) as Snippet & HTMLCollection;
+            const mixedSnippet = propertySnippetFactory(this._getStorage('children')) as Snippet & HTMLCollection;
 
             const children = super.children;
 
@@ -91,15 +88,26 @@ export function makeCustomComponent(baseClass: new () => HTMLElement): new () =>
                 }
             });
         }
+
+        private _getStorage(property: string): HTMLDivElement {
+            if (!this._snippetStorage[property]) {
+                this._snippetStorage[property] = document.createElement('div');
+                if (property !== 'children') {
+                    (this as any)[property] = propertySnippetFactory(this._snippetStorage[property]);
+                }
+            }
+            return this._snippetStorage[property];
+        }
     };
 }
 
-function propertySnippetFactory(storage: HTMLSpanElement): Snippet {
+function propertySnippetFactory(storage: HTMLDivElement): Snippet {
     return createRawSnippet(() => {
         return {
-            render: () => `<span></span>`,
+            render: () => `<div class="hawk-custom-element"></div>`,
             setup: (target) => {
                 target.append(...storage.childNodes);
+                console.log('CREATED WRAPPER', target);
                 return () => {
                     storage.append(...target.childNodes);
                 };
