84c8b9c72e69a865b782b5d03b345625872fe8a3
neunerlei <ich@neunerlei.eu>
2025-06-25 14:29:11 +0200

    fix: fix vaul version so it works with svelte 5

--- src/lib/drawer/Drawer.svelte
+++ src/lib/drawer/Drawer.svelte
@@ -3,6 +3,10 @@
     import style from './Drawer.module.sass';
     import Headline from '$lib/headline/Headline.svelte';
     import Icon from '$lib/icon/Icon.svelte';
+    import { mergeProps } from "$lib/util/mergeProps.js";
+    import * as Vaul from "vaul-svelte";
+
+    const Drawer = Vaul.Drawer;
 
     export interface Props extends HTMLAttributes<HTMLDivElement> {
         heading?: string;
@@ -18,23 +22,21 @@
     }: Props = $props();
 </script>
 
-<div class={style.drawer}>
-    <Drawer.Root>
-        <Drawer.Trigger>Open</Drawer.Trigger>
+<Drawer.Root direction="bottom">
+    <Drawer.Trigger>Open</Drawer.Trigger>
 
-        <Drawer.Portal>
-            <Drawer.Overlay class={style.drawer_overlay} />
-            <Drawer.Content class={style.drawer_content}>
-                <div class={style.drawer_drag}></div>
-                <div class={style.drawer_title}>
-                    <Headline>{heading ? heading : ""}</Headline>
-                    <Drawer.Close class={style.drawer_close}><Icon icon="close" size="large"></Icon></Drawer.Close
-                    >
-                </div>
-                <div class={style.drawer_body}>
-                    {@render children?.()}
-                </div>
-            </Drawer.Content>
-        </Drawer.Portal>
-    </Drawer.Root>
-</div>
+    <Drawer.Portal>
+        <Drawer.Overlay class={style.drawer_overlay} />
+        <Drawer.Content class={style.drawer_content}>
+            <div class={style.drawer_drag}></div>
+            <div class={style.drawer_title}>
+                <Headline>{heading ? heading : ""}</Headline>
+                <Drawer.Close class={style.drawer_close}><Icon icon="close" size="large"></Icon></Drawer.Close
+                >
+            </div>
+            <div class={style.drawer_body}>
+                {@render children?.()}
+            </div>
+        </Drawer.Content>
+    </Drawer.Portal>
+</Drawer.Root>
